372a484f9ccd7199c4801412a0e3478aa4f5231a
rgioeli <rgioeli@icloud.com>
2023-01-22 18:26:09 -0500

    first upload

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,20 @@
-import './globals.css'
+"use client";
 
-export default function RootLayout({ children }) {
+import Image from "next/image";
+import "./globals.css";
+import { BsCartFill, BsPersonFill } from "react-icons/bs";
+import Link from "next/link";
+import { Quicksand } from "@next/font/google";
+import { SessionProvider } from "next-auth/react";
+import MainContextProvider from "../context/Main";
+import { MainContext } from "../context/Main";
+import { useContext } from "react";
+import CartCount from "./CartCount";
+
+// const quicksand = Quicksand({ weight: ["400", "700"], subsets: ["latin"] });
+// import MainContextProvider from "../context/Main";
+
+export default function RootLayout({ children, session }) {
   return (
     <html lang="en">
       {/*
@@ -8,7 +22,16 @@ export default function RootLayout({ children }) {
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={`text-[#313131]`}>
+        <SessionProvider session={session}>
+          <MainContextProvider>
+            <div className="container m-auto">
+              <CartCount />
+              {children}
+            </div>
+          </MainContextProvider>
+        </SessionProvider>
+      </body>
     </html>
-  )
+  );
 }
